8c6a16a1be91a0f6585e95e407c5c03c23ec68bc
hzaimi4179-coder <h.zaimi4179@uca.ac.ma>
2025-11-05 21:03:49 +0100

    Add files via upload

---
+++ TP_REST/src/main/java/ma/rest/spring/entities/Client.java
@@ -0,0 +1,26 @@
+package ma.rest.spring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "comptes")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String email;
+
+    @OneToMany(mappedBy = "client")
+    private List<Compte> comptes;
+}
+
---
+++ TP_REST/src/main/java/ma/rest/spring/repositories/CompteRepository.java
@@ -0,0 +1,18 @@
+package ma.rest.spring.repositories;
+
+import ma.rest.spring.entities.Compte;
+import ma.rest.spring.entities.TypeCompte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+
+    //  Recherche personnalisée par type de compte
+    @RestResource(path = "/byType")
+    List<Compte> findByType(@Param("t") TypeCompte type);
+}
